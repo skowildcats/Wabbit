c88a00ab53ecfcf62eb9f7bd69066ed0bc3f712f
webbdevv <kyle.xu4@gmail.com>
2021-07-19 07:54:11 -0700

    Imgs structure

--- app.js
+++ app.js
@@ -7,14 +7,46 @@ const passport = require('passport');
 const users = require('./routes/api/users')
 require('./config/passport')(passport)
 
-
-
+//attaching files to mongo
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
-  
+
+// Init gfs
+let gfs;
+mongoose.connection.once('open', () => {
+  gfs = Grid(mongoose.connection.db, mongoose.mongo)
+  gfs.collection('icons')
+})
+
+// Create storage engine
+const storage = new GridFsStorage({
+  url: mongoURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads'
+        };
+        resolve(fileInfo);
+      });
+    });
+  }
+});
+const upload = multer({ storage });
+
+app.get("/", (req, res) => res.send("Hello World!!!"));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
