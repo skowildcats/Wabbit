b1fd68da4615c18c94372c76fadfd04fcadb29c3
theonewei <wei.n.isaac@gmail.com>
2021-07-19 13:02:09 -0400

    can get individual tasks and update them too

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -18,18 +18,24 @@ router.post('/new',(req,res)=>{
 })
 
 // update task route
-router.post('/:taskId',(req,res)=>{
-  task = Task.findById(req.params.taskId)
-  for(field in req){
-    task[field] = req[field]
-  }
-  task.save()
+router.put('/:taskId',(req,res)=>{
+  Task.findById(req.params.taskId)
+    .then(task=>{
+      for(field in req.body){
+        task[field] = req.body[field]
+      }
+      task.save()
+      res.json({task: task})
+    })
+    .catch(err=>res.status(404).json({error: err}))
+  
 })
 
 // get a single task
 router.get('/:taskId',(req,res)=>{
-  task = Task.findById(req.params.taskId)
-  res.json(task)
+  Task.findById(req.params.taskId)
+    .then(task=>res.json(task))
+    .catch(err=>res.status(404).json({error: err}))
 })
 
 // get all tasks
@@ -40,3 +46,8 @@ router.get('/',(req,res)=>{
 })
 
 module.exports = router
+
+// user: 60f588bb3ee3cb100f85728c
+// task1: 60f5aa5dbebcb620da43d32f
+// task2: 60f5ab937372de2144b5f2d0
+// token: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwZjU4OGJiM2VlM2NiMTAwZjg1NzI4YyIsInVzZXJuYW1lIjoiam8iLCJpYXQiOjE2MjY3MTI1ODgsImV4cCI6MTYyNjcxNjE4OH0.bf_A9qfXNedVktiyaPWDOvstUsBkoSS6IONYHJXzq0k
