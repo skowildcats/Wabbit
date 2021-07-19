16042c19e3b3b4f200276209539d29a81d444e2f
theonewei <wei.n.isaac@gmail.com>
2021-07-19 10:45:53 -0400

    User auth (#3)
    * start user auth
    
    * setup user model schema
    
    * add token to user auth
    
    * return current user

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const users = require('./routes/api/users')
+require('./config/passport')(passport)
 
 
 
@@ -12,11 +14,13 @@ mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
-
-app.get("/", (req, res) => res.send("Hello World!!!"));
-
+  
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(passport.initialize())
+app.use('/api/users',users)
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
