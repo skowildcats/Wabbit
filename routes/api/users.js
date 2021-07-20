5b9327f00a0fc688ad1c30907fc93dcc262d034b
theonewei <wei.n.isaac@gmail.com>
2021-07-20 12:26:16 -0400

    add validation and token to user routes (#16)

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,19 +5,28 @@ const User = require('../../models/User')
 const jwt = require('jsonwebtoken')
 const keys = require('../../config/keys')
 const passport = require('passport')
+const validateRegisterInput = require('../../validation/register')
+const validateLoginInput = require('../../validation/login')
 
 //route for signing up
 
 router.post('/register', (req,res) => {
+
+  const{errors,isValid} = validateRegisterInput(req.body)
+  if(!isValid){
+    return res.status(400).json(errors)
+  }
+
   User.findOne({email: req.body.email})
     .then(user=>{
       if(user){
         return res.status(400).json({email: 'email has already been taken'})
       } else {
         const newUser = new User({
-          username: req.body.username,
           email: req.body.email,
-          password: req.body.password
+          password: req.body.password,
+          firstName: req.body.firstName,
+          lastName: req.body.lastName
         })
 
         bcrypt.genSalt(10, (err, salt) => {
@@ -33,9 +42,63 @@ router.post('/register', (req,res) => {
     })
 })
 
+// route for updating password
+
+router.put('/password',passport.authenticate('jwt', {session: false}),(req,res)=>{
+
+  const email = req.body.email
+  const password = req.body.password
+
+  User.findOne({email})
+    .then(user=>{
+      // check if user exists
+      if(!user){
+        res.json({error: "user couldknt be found XD"})
+      }
+      //check that password is correct
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if(isMatch){
+            bcrypt.genSalt(10,(err,salt) =>{
+              bcrypt.hash(newPassword,salt,(err,hash)=>{
+                if (err) throw err
+                user.password = hash
+                user.save()
+                  .then(user => res.json(user))
+                  .catch(err => res.json({errors: err}))
+              })
+            })
+          }else{
+            res.json({error: 'Incorrect Password'})
+          }
+        })
+    })
+})
+
+// route for updating name
+
+router.put('/info', passport.authenticate('jwt', {session: false}), (req,res)=>{
+  const {email,firstName,lastName} = req.body
+
+  User.findOne({email})
+    .then(user=>{
+      user.firstName = firstName
+      user.lastName = lastName
+      user.save()
+        .then(res.json(user))
+        .catch(console.log)
+    })
+})
+
 //route for logging in
 
 router.post('/login', (req, res) => {
+
+  const{errors, isValid} = validateLoginInput(req.body)
+  if(!isValid){
+    return res.status(400).json(errors)
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
