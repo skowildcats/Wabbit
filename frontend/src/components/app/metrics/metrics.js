f9d8a17d0e248c67aee4ad42f7df49a79a17c22c
webbdevv <kyle.xu4@gmail.com>
2021-07-21 12:52:39 -0700

    commit before hotfixing menu

--- frontend/src/components/app/metrics/metrics.js
+++ frontend/src/components/app/metrics/metrics.js
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from 'react'
+import moment from 'moment'
 
 export default function Metrics(props){
   useEffect(() => {
     props.fetchMetrics(props.userId)
   }, [])
+  
+  let startOfWeek = moment().startOf('isoweek').format('MMM Do YYYY')
+  let endOfWeek = moment().endOf('isoweek').format('MMM Do YYYY')
   return (
     <div className="metrics-page">
       <div className="header">
-        This will feature data for a basic habit over the last week
+        <div className="this-week">Week of {startOfWeek} - {endOfWeek}</div>
+        <div className="habit">
+          
+        </div>
       </div>
       <div className="bar-graph">
         This will have all data for the bar graph for all habits/tasks over the past 7 days
