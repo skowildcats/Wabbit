2e846a4b274af8c89aec545cd6057ed4564acbf0
theonewei <wei.n.isaac@gmail.com>
2021-07-21 15:01:29 -0400

    tasks and metrics refactor to utilize async await

--- routes/api/metrics.js
+++ routes/api/metrics.js
@@ -3,17 +3,19 @@ const router = express.Router()
 const Task = require('../../models/Task')
 const metricsUtil = require('../util/metrics_util')
 
-router.get('/:userId',(req,res)=>{
-  Task.find({user: req.params.userId})
-    .then(tasks=>{
-      const range = req.body.range||999
-      res.json({
-        percentComplete: metricsUtil.percentComplete(tasks,range),
-        percentOnTime: metricsUtil.percentOnTime(tasks,range),
-        completedByWeekday: metricsUtil.completedByWeekday(tasks,range)
-      })
+router.get('/:userId',async (req,res)=>{
+  try {
+    const tasks = await Task.find({user: req.params.userId})
+    const range = req.body.range||999
+
+    res.json({
+      percentComplete: metricsUtil.percentComplete(tasks,range),
+      percentOnTime: metricsUtil.percentOnTime(tasks,range),
+      completedByWeekday: metricsUtil.completedByWeekday(tasks,range)
     })
-    .catch(console.log)
+  } catch(error){
+    console.log(error)
+  }
 })
 
 module.exports = router
