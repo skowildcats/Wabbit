54c7343331770a4e98e0069411c6170b2e32fa0c
webbdevv <kyle.xu4@gmail.com>
2021-07-21 12:18:46 -0700

    Added basic layout

--- frontend/src/components/app/metrics/metrics.js
+++ frontend/src/components/app/metrics/metrics.js
@@ -6,7 +6,16 @@ export default function Metrics(props){
   }, [])
   return (
     <div className="metrics-page">
-      Metrics page
+      <div className="header">
+        This will feature data for a basic habit over the last week
+      </div>
+      <div className="bar-graph">
+        This will have all data for the bar graph for all habits/tasks over the past 7 days
+      </div>
+
+      <div className="line-graph">
+        This will track much longer term task data
+      </div>
     </div>
   )
 }
