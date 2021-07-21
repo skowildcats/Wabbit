bbfecc3c830d32cfee102b8c2561c7d1bd67f1d2
webbdevv <kyle.xu4@gmail.com>
2021-07-21 13:50:09 -0700

    Icons

--- frontend/src/components/app/metrics/metrics.js
+++ frontend/src/components/app/metrics/metrics.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import moment from 'moment'
 
 export default function Metrics(props){
@@ -6,6 +6,9 @@ export default function Metrics(props){
     props.fetchMetrics(props.userId)
   }, [])
   
+  let habitStyle = { //later we wll dynamically style the habits
+
+  }
   let startOfWeek = moment().startOf('isoweek').format('MMM Do YYYY')
   let endOfWeek = moment().endOf('isoweek').format('MMM Do YYYY')
   return (
@@ -13,7 +16,16 @@ export default function Metrics(props){
       <div className="header">
         <div className="this-week">Week of {startOfWeek} - {endOfWeek}</div>
         <div className="habit">
-          
+          <img src="" alt="" className="icon" />
+          <p>Habit Name</p>
+          <label className="toggle-button">
+            <input type="checkbox" />
+             <div>
+              <svg viewBox="0 0 44 44">
+                  <path d="M14,24 L21,31 L39.7428882,11.5937758 C35.2809627,6.53125861 30.0333333,4 24,4 C12.95,4 4,12.95 4,24 C4,35.05 12.95,44 24,44 C35.05,44 44,35.05 44,24 C44,19.3 42.5809627,15.1645919 39.7428882,11.5937758" transform="translate(-2.000000, -2.000000)"></path>
+              </svg>
+            </div>
+          </label>
         </div>
       </div>
       <div className="bar-graph">
