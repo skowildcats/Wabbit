0d65bf0a9d23e4e661909f71c9ec641d0842f51e
webbdevv <kyle.xu4@gmail.com>
2021-07-21 14:43:02 -0700

    Added formatting before going back to main branch

--- frontend/src/components/app/metrics/metrics.js
+++ frontend/src/components/app/metrics/metrics.js
@@ -16,16 +16,27 @@ export default function Metrics(props){
       <div className="header">
         <div className="this-week">Week of {startOfWeek} - {endOfWeek}</div>
         <div className="habit">
-          <img src="" alt="" className="icon" />
+          <img src="" alt="Icon-placeholder" className="icon" />
           <p>Habit Name</p>
-          <label className="toggle-button">
-            <input type="checkbox" />
-             <div>
-              <svg viewBox="0 0 44 44">
-                  <path d="M14,24 L21,31 L39.7428882,11.5937758 C35.2809627,6.53125861 30.0333333,4 24,4 C12.95,4 4,12.95 4,24 C4,35.05 12.95,44 24,44 C35.05,44 44,35.05 44,24 C44,19.3 42.5809627,15.1645919 39.7428882,11.5937758" transform="translate(-2.000000, -2.000000)"></path>
-              </svg>
-            </div>
-          </label>
+          <div className="checkboxes">
+            <label className="toggle-button">Monday
+              <input type="checkbox" />
+              <div>
+                <svg viewBox="0 0 44 44">
+                    <path d="M14,24 L21,31 L39.7428882,11.5937758 C35.2809627,6.53125861 30.0333333,4 24,4 C12.95,4 4,12.95 4,24 C4,35.05 12.95,44 24,44 C35.05,44 44,35.05 44,24 C44,19.3 42.5809627,15.1645919 39.7428882,11.5937758" transform="translate(-2.000000, -2.000000)"></path>
+                </svg>
+              </div>
+            </label>
+            <label className="toggle-button">Tuesday
+              <input type="checkbox" />
+              <div>
+                <svg viewBox="0 0 44 44">
+                    <path d="M14,24 L21,31 L39.7428882,11.5937758 C35.2809627,6.53125861 30.0333333,4 24,4 C12.95,4 4,12.95 4,24 C4,35.05 12.95,44 24,44 C35.05,44 44,35.05 44,24 C44,19.3 42.5809627,15.1645919 39.7428882,11.5937758" transform="translate(-2.000000, -2.000000)"></path>
+                </svg>
+              </div>
+            </label>
+          </div>
+          {/* repeat label for each day of the week */}
         </div>
       </div>
       <div className="bar-graph">
