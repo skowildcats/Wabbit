efd648c151f59fe60cc9b79d11924d7afbbb6909
webbdevv <kyle.xu4@gmail.com>
2021-07-22 13:41:49 -0700

    Complete metrics skeleton with basic styling

--- frontend/src/components/app/metrics/metrics_container.js
+++ frontend/src/components/app/metrics/metrics_container.js
@@ -1,15 +1,20 @@
 import { connect } from 'react-redux';
 import { fetchMetrics } from '../../../actions/metrics_actions';
+import { fetchHabits } from '../../../actions/habit_actions';
+import { fetchTasks } from '../../../actions/task_actions';
 import Metrics from './metrics';
 
 const mSTP = state => ({
   userId: state.session.user.id,
   metrics: state.metrics,
-  tasks: state.entities.tasks
+  tasks: state.entities.tasks,
+  habits: Object.values(state.entities.habits)
 })
 
 const mDTP = (dispatch, ownProps) => ({
   fetchMetrics: (userId = ownProps.userId) => dispatch(fetchMetrics(userId)),
+  fetchHabits: (userId) => dispatch(fetchHabits(userId)),
+  fetchTasks: (userId) => dispatch(fetchTasks(userId))
 })
 
 export default connect(mSTP, mDTP)(Metrics)
