d9da09bc74ffaab14370d662d9da4799bc47aab5
theonewei <wei.n.isaac@gmail.com>
2021-07-21 21:40:40 -0400

    finish backend for habits, habits now autopopulate tasks

--- routes/api/habits.js
+++ routes/api/habits.js
@@ -1,42 +1,25 @@
 const express = require('express')
 const router = express.Router()
 const Habit = require('./../../models/Habit')
-const TaskUtil = require('../util/tasks_util')
+const taskUtil = require('../util/tasks_util')
 
 router.post('/new',async (req,res)=>{
-  const newHabit = {}
+  const habitOptions = {}
   for(field in req.body){
-    newHabit[field] = req.body[field]
+    habitOptions[field] = req.body[field]
   }
+  const newHabit = new Habit(habitOptions)
   await newHabit.save()
-  if(appliesToday(newHabit))
+  if(taskUtil.appliesToday(newHabit)){
+    taskUtil.createTaskFromHabit(newHabit)
+  }
   res.json(newHabit)
 })
 
-function appliesToday(habit){
-  const now = new Date()
-  switch (habit.recurrence) {
-    case 'd':
-      return true
-    case 'w':
-      if(daysOfTheWeek[(now.getDay()+6)%7]==='_') return false
-      return true
-    case 'm':
-      if(lastDayOfMonth(now) && 
-        now.getDate()<habit.createdAt.getDate()){
-          return true
-      }
-      return now.getDate() === habit.createdAt.getDate()
-  }
-}
-
-function lastDayOfMonth(date){
-  const newDate = new Date(date)
-  newDate.setDate(newDate.getDate()+1)
-  if(date.getMonth() = newDate.getMonth()) return false
-  return true
-}
-
+router.get('/all/:userId', async (req,res)=>{
+  const habits = await Habit.find({user: req.params.userId})
+  res.json(habits)
+})
 
 
 module.exports = router
--- routes/util/tasks_util.js
+++ routes/util/tasks_util.js
@@ -2,13 +2,37 @@ const Task = require('../../models/Task')
 
 exports.createTaskFromHabit = async function(habit){
   const newTask = new Task({
-    title: req.body.title,
-    user: req.body.user,
-    description: req.body.description,
-    dueDate: req.body.dueDate,
-    color: req.body.color,
-    icon: req.body.icon,
+    title: habit.title,
+    user: habit.user,
+    description: habit.description,
+    dueDate: habit.dueDate,
+    color: habit.color,
+    icon: habit.icon,
   });
   await newTask.save();
   return newTask
+}
+
+exports.appliesToday = function(habit){
+  const now = new Date()
+  switch (habit.recurrence) {
+    case 'Daily':
+      return true
+    case 'Weekly':
+      if(daysOfTheWeek[(now.getDay()+6)%7]==='_') return false
+      return true
+    case 'Monthly':
+      if(lastDayOfMonth(now) && 
+        now.getDate()<habit.createdAt.getDate()){
+          return true
+      }
+      return now.getDate() === habit.createdAt.getDate()
+  }
+}
+
+function lastDayOfMonth(date){
+  const newDate = new Date(date)
+  newDate.setDate(newDate.getDate()+1)
+  if(date.getMonth() = newDate.getMonth()) return false
+  return true
 }
