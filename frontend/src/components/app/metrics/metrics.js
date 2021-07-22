5ec91df5605cf7f2731f2db910172ef63e6e45c4
webbdevv <kyle.xu4@gmail.com>
2021-07-22 14:09:32 -0700

    Refactor backend to allow for better habit tracking in metrics

--- frontend/src/components/app/metrics/metrics.js
+++ frontend/src/components/app/metrics/metrics.js
@@ -3,52 +3,46 @@ import moment from 'moment'
 import BarGraph from './bar_graph'
 import LineGraph from './line_graph'
 import PieChart from './pie_chart'
+import Checkbox from './checkbox'
 import { setupBarGraphData, setupLineGraphData, setupPieCount, setupPieComplete } from './metrics_util';
 
 export default function Metrics(props){
-  window.moment = moment
   const [loading, setLoading] = useState(true)
   useEffect(() => {
-    Promise.all([props.fetchMetrics(props.userId), props.fetchHabits(props.userId), props.fetchTasks(props.userId)]).then(data => {
+    props.fetchMetrics(props.userId).then(data => {
       setLoading(false)
     })
   }, []);
   let habitStyle = { //later we wll dynamically style the habits
 
   }
+
+  //this page probably will need a loading page later
   if(loading) return null;
+
+  //grab start and end dates for this week for the habit bar
   let startOfWeek = moment().startOf('isoweek').format('MMM Do YYYY')
   let endOfWeek = moment().endOf('isoweek').format('MMM Do YYYY')
 
-  console.log(props)
-  let habit;
   let progressTracker;
   //the idea is to take a single habit, will need to alter this later
-  if(props.habits){
-    habit = props.habits[1];
+  if(props.metrics.habit){
     let weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
     let currentDay = moment().format('dddd');
     currentDay = weekdays.indexOf(currentDay)
     //HEY A/A!!!
     progressTracker = weekdays.map((day, idx) => {
-      return <label key={day} className="toggle-button">
-        <span>{day}</span>
-        <input type="checkbox" disabled={idx > currentDay ? true : false}/>
-        <div>
-          <svg viewBox="0 0 44 44">
-              <path d="M14,24 L21,31 L39.7428882,11.5937758 C35.2809627,6.53125861 30.0333333,4 24,4 C12.95,4 4,12.95 4,24 C4,35.05 12.95,44 24,44 C35.05,44 44,35.05 44,24 C44,19.3 42.5809627,15.1645919 39.7428882,11.5937758" transform="translate(-2.000000, -2.000000)"></path>
-          </svg>
-        </div>
-      </label>
+      return <Checkbox key={day} disabled={idx > currentDay ? true : false} day={day}/>
     })
   }
+  
   return (
     <div className="metrics-page">
       <div className="header">
         <div className="this-week">Week of {startOfWeek} - {endOfWeek}</div>
         <div className="habit">
-          <img src={`/api/files/image/${habit.icon}`} alt="Icon-placeholder" className="icon" />
-          <p>{habit ? habit.title : "Habit"}</p>
+          <img src={`/api/files/image/${props.metrics.habit.icon}`} alt="Icon-placeholder" className="icon" />
+          <p>{props.metrics ? props.metrics.habit.title : "Habit"}</p>
             <div className="checkboxes">
               {progressTracker}
             </div>
--- routes/api/metrics.js
+++ routes/api/metrics.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Task = require('../../models/Task')
 const metricsUtil = require('../util/metrics_util')
+const Habit = require('../../models/Habit')
 
 router.get('/:userId',async (req,res)=>{
   try {
@@ -14,13 +15,20 @@ router.get('/:userId',async (req,res)=>{
       await Task.find({user: req.params.userId}),70)
     
     let count = await Task.countDocuments({user: req.params.userId})
+    let habit = await Habit.findOne({user: req.params.userId, recurrence: "Daily"});
+    let habitTasks = metricsUtil.filterByStartDate(
+      await Task.find({habit: habit._id}),
+      7
+    );
     res.json({
       taskDonePerWeek: metricsUtil.tasksDonePerWeek(past10Weeks),
       lateByWeekday: metricsUtil.lateByWeekday(tasks),
       onTimeByWeekday: metricsUtil.onTimeByWeekday(tasks),
       percentComplete: metricsUtil.percentComplete(tasks),
       percentOnTime: metricsUtil.percentOnTime(tasks),
-      count: count
+      count: count,
+      habit,
+      habitTasks
     })
   } catch(error){
     console.log(error)
