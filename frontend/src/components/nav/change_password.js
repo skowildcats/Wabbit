4ecbcd8280f9ea6b1372d97a8437e2e84e775b4f
theonewei <wei.n.isaac@gmail.com>
2021-09-02 18:29:34 -0400

    got rid of email field in the update password form

--- frontend/src/components/nav/change_password.js
+++ frontend/src/components/nav/change_password.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 
 export default function ChangePasswordModal(props) {
   const [credentials, setCredentials] = useState({
-    email: '',
+    email: props.email,
     currentPassword: '',
     newPassword: '',
     confirmPassword: ''
@@ -26,11 +26,11 @@ export default function ChangePasswordModal(props) {
   }
 
   if(!props.open) return null;
-  const fields = ["email", "currentPassword", "newPassword", "confirmPassword"].map(field => 
+  const fields = ["currentPassword", "newPassword", "confirmPassword"].map(field => 
     <div className="form-field" key={field}>
       {errors[field] ? <p className="pass-errors">{errors[field]}</p> : null}
       <label htmlFor="">{field.replace(/([A-Z])/g, ' $1').replace(/^./, function(str){ return str.toUpperCase(); })}</label>
-      <input type={field === "email" ? "text" : "password"} onChange={(e) => setCredentials({...credentials, [field]: e.target.value})}/>
+      <input type={"password"} onChange={(e) => setCredentials({...credentials, [field]: e.target.value})}/>
     </div>
     )
   return (
