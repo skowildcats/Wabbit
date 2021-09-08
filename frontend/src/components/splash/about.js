fa643a856c34931b23bc2d8bdc83cae90f4e7d51
Isaac Wei <wei.n.isaac@gmail.com>
2021-09-08 13:19:00 -0400

    Contact links (#138)
    * fix pie chart rendering
    
    * metrics were getting returned before user was getting returned
    
    * task creation validations are returning error messages
    
    * fix habit validations being sent as errors to the front end
    
    * make task colors appear correctly
    
    * remove logger
    
    * remove console logs
    
    * make kyle photo papear
    
    * make contact links open in new tab

--- frontend/src/components/splash/about.js
+++ frontend/src/components/splash/about.js
@@ -16,13 +16,13 @@ export default class About extends React.Component {
           <div className="team-member">
             <img className="team-pic" src={process.env.PUBLIC_URL + "/team/kyle.jpg"} />
             <p className="name">KYLE</p>
-            <a href="https://github.com/webbdevv/" className="contact-link">
+            <a href="https://github.com/webbdevv/" target="_blank" className="contact-link">
               <img id="github" src={process.env.PUBLIC_URL + "/github.png"} alt="github" />
             </a>
-            <a href="https://www.linkedin.com/in/kyle-xu/" className="contact-link">
+            <a href="https://www.linkedin.com/in/kyle-xu/" target="_blank" className="contact-link">
               <img id="linkedin" src={process.env.PUBLIC_URL + "/linkedin.png"} alt="linkedin" />
             </a>
-            <a href="https://angel.co/u/kyle-xu-1/" className="contact-link">
+            <a href="https://angel.co/u/kyle-xu-1/" target="_blank" className="contact-link">
               <img id="linkedin" src={process.env.PUBLIC_URL + "/angellist.png"} alt="linkedin" />
             </a>
             <p className="team-role">TEAM LEAD</p>
@@ -30,13 +30,13 @@ export default class About extends React.Component {
           <div className="team-member">
             <img className="team-pic" src={process.env.PUBLIC_URL + "/team/joseph.png"} />
             <p className="name">JOSEPH</p>
-            <a href="https://github.com/josephwyang/" className="contact-link">
+            <a href="https://github.com/josephwyang/" target="_blank" className="contact-link">
               <img id="github" src={process.env.PUBLIC_URL + "/github.png"} alt="github" />
             </a>
-            <a href="https://www.linkedin.com/in/josephwyang/" className="contact-link">
+            <a href="https://www.linkedin.com/in/josephwyang/" target="_blank" className="contact-link">
               <img id="linkedin" src={process.env.PUBLIC_URL + "/linkedin.png"} alt="linkedin" />
             </a>
-            <a href="https://angel.co/u/josephwyang/" className="contact-link">
+            <a href="https://angel.co/u/josephwyang/" target="_blank" className="contact-link">
               <img id="linkedin" src={process.env.PUBLIC_URL + "/angellist.png"} alt="linkedin" />
             </a>
             <p className="team-role">FRONTEND LEAD</p>
@@ -44,13 +44,13 @@ export default class About extends React.Component {
           <div className="team-member">
             <img className="team-pic" src={process.env.PUBLIC_URL + "/team/jon.png"} />
             <p className="name">JON</p>
-            <a href="https://github.com/skowildcats/" className="contact-link">
+            <a href="https://github.com/skowildcats/" target="_blank" className="contact-link">
               <img id="github" src={process.env.PUBLIC_URL + "/github.png"} alt="github" />
             </a>
-            <a href="https://www.linkedin.com/in/jon-chen67/" className="contact-link">
+            <a href="https://www.linkedin.com/in/jon-chen67/" target="_blank" className="contact-link">
               <img id="linkedin" src={process.env.PUBLIC_URL + "/linkedin.png"} alt="linkedin" />
             </a>
-            <a href="https://angel.co/u/jonathan-chen-30/" className="contact-link">
+            <a href="https://angel.co/u/jonathan-chen-30/" target="_blank" className="contact-link">
               <img id="linkedin" src={process.env.PUBLIC_URL + "/angellist.png"} alt="linkedin" />
             </a>
             <p className="team-role">FLEX DEVELOPER</p>
@@ -58,13 +58,13 @@ export default class About extends React.Component {
           <div className="team-member">
             <img className="team-pic" src={process.env.PUBLIC_URL + "/team/isaac.png"} />
             <p className="name">ISAAC</p>
-            <a href="https://github.com/theonewei/" className="contact-link">
+            <a href="https://github.com/theonewei/" target="_blank" className="contact-link">
               <img id="github" src={process.env.PUBLIC_URL + "/github.png"} alt="github" />
             </a>
-            <a href="https://www.linkedin.com/in/isaac-wei-9945a2105/" className="contact-link">
+            <a href="https://www.linkedin.com/in/isaac-wei-9945a2105/" target="_blank" className="contact-link">
               <img id="linkedin" src={process.env.PUBLIC_URL + "/linkedin.png"} alt="linkedin" />
             </a>
-            <a href="https://angel.co/u/isaac-wei/" className="contact-link">
+            <a href="https://angel.co/u/isaac-wei/" target="_blank" className="contact-link">
               <img id="linkedin" src={process.env.PUBLIC_URL + "/angellist.png"} alt="linkedin" />
             </a>
             <p className="team-role">BACKEND LEAD</p>
